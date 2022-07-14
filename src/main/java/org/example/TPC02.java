e763fd4fed282a19ac5fb37eab4f018193137fe0
online5880 <gjtjqkr1@naver.com>
2022-07-14 23:00:53 +0900

    프로그래밍의 3대 요소(변수, 자료형, 할당)

---
+++ src/main/java/org/example/TPC02.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class TPC02 {
+    public static void main(String[] args) {
+        // 프로그래밍의 3대요소 : 변수, 자로형(DataType), 할당(=)
+
+        int a,b,c;
+
+        float f; // float, double
+        f = 3.45f;
+        System.out.println(f);
+
+        char d; // 캐릭터 타입
+        d = 'A'; // 따옴표 하나
+        System.out.println(d);
+    }
+}
