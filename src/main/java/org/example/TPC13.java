0917cff828f68bb434d40b01c7d7ccf9e9fdbfac
online5880 <gjtjqkr1@naver.com>
2022-07-18 23:27:34 +0900

    private 생성자 (static과 관계)

---
+++ src/main/java/org/example/TPC13.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import kr.tpc.Inflearn;
+
+public class TPC13 {
+    public static void main(String[] args) {
+
+        //Inflearn inf = new Inflearn();
+        //inf.tpc();
+        Inflearn.tpc();
+        //inf.java();
+        Inflearn.java();
+
+        // * Java API 생성자 private
+        System.out.println();
+    }
+}
