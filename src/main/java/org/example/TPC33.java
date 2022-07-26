3dd04eb42fe5f9db5672a81ed07fc6271f65c3aa
online5880 <gjtjqkr1@naver.com>
2022-07-26 23:02:04 +0900

    package(패키지)

---
+++ src/main/java/org/example/TPC33.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import kr.inflearn.MyClass;
+
+public class TPC33 {
+    public static void main(String[] args) {
+        MyClass my = new MyClass();
+        int v = my.sum(10,100);
+        System.out.println(v);
+    }
+}
